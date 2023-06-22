cbf6a159528b239e0afd796b6e18fbfd48c1f1b2
chammyae24 <chammyae.zawaud@gmail.com>
2023-06-22 11:22:16 +0630

    update socket working process

--- server/src/graphql/types.ts
+++ server/src/graphql/types.ts
@@ -1,8 +1,5 @@
 import { PrismaClient, user } from "@prisma/client";
-import { createPubSub } from "graphql-yoga";
 import { Pubsub } from "../yoga";
-
-const pubsub = createPubSub();
 
 export type GraphQLContext = {
   prisma: PrismaClient;
